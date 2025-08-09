b278f8e02fc42f0e4c8f6142ed7cf78e227f3445
AshenChromatic <ashenchromatic@gmail.com>
2025-08-09 00:17:11 -0600

    New shop tag exists and becomes available with enough research (though it is currently empty) + reorganized some of the code

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
+let DEBUG_MODE = true; // Set to false for normal gameplay
+
+
+//This function is for waiiiting
 function sleep(ms) {
+    if (DEBUG_MODE) return Promise.resolve();
     return new Promise(function(resolve) {
         setTimeout(resolve, ms);
     });
 }
 
+//Put text in the log
 function sendToLog(message) {
     const logDiv = document.getElementById('gameLog');
     logDiv.innerHTML += '> ' + message + '<br>';
 }
 
+
+//Setup for orbUnlock
+let orbUnlock = false;
+
+//Make knowledge go up when the research button is clicked
+//Also make the knowledge display visible if it isn't already
 let knowledge = 0;
 let knowledgeVisible = false;
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function researchClick() {
+    //Knowledge visiblizer
     if (!knowledgeVisible) {
         const knowledgeDisplay = document.querySelector('.knowledgeDisplay');
         if (knowledgeDisplay) {
@@ -19,22 +37,83 @@ function researchClick() {
             knowledgeVisible = true;
         }
     }
+    //Knowledge increaserizer
     knowledge += 1;
     const knowledgeAmount = document.getElementById('knowledgeAmount');
     if (knowledgeAmount !== null) {
     knowledgeAmount.innerHTML = knowledge;
     }
+    //Orb unlocker
+    if (orbUnlock === false && knowledge >=10) {
+        let orbUnlockRandom = getRandomInt(1, 30);
+        console.log('orbUnlockRandom: ' + orbUnlockRandom);
+        //If the random number is 1, unlock the orb
+        if (orbUnlockRandom === 1) {
+            orbUnlock = true;
+            unlockOrb();
+        }
+    }
 }
 
+//---------------------------//
+// Tab Switchers             //
+//---------------------------//
+
+const tabIds = ['researchTab', 'shopTab'];
+
+function openTab(tabId) {
+    tabIds.forEach(id => {
+        const tab = document.getElementById(id);
+        if (tab) {
+            tab.classList.remove('show');
+        }
+    });
+    const currentTab = document.getElementById(tabId);
+    if (currentTab) {
+        currentTab.classList.add('show');
+    }
+}
+
+
+//---------------------------//
+// Dialogs                   //
+//---------------------------//
+
+// Orb unlock dialog
+async function unlockOrb() {
+    sendToLog('After a few minutes of research, you feel like you\'ve learned something.');
+    await sleep(4000);
+    sendToLog('To use magic, you need mana. You do not currently have any mana.')
+    await sleep(4000);
+    sendToLog('Luckily, they sell mana orbs online nowadays.');
+    await sleep(4000);
+    sendToLog('You think that maybe you should buy one.');
+    document.getElementById('researchTabBtn').classList.add('show');
+    document.getElementById('shopSpace').classList.add('show');
+    document.getElementById('shopTabBtn').classList.add('show');
+}
+
+//---------------------------//
+// Event Listeners           //
+//---------------------------//
+
+//Tabs
+document.getElementById('researchTabBtn').addEventListener('click', function() {
+    openTab('researchTab');
+});
+document.getElementById('shopTabBtn').addEventListener('click', function() {
+    openTab('shopTab');
+});
+
+//Research button
 document.getElementById('researchBtn').addEventListener('click', researchClick);
 
 
+//---------------------------//
+// Initialization            //
+//---------------------------//
 
-
-
-
-
-
+//Initial log and display Research button
 async function startGame() {
     sendToLog('You sit at your desk, the white-ish light of your computer monitor illuminating your face.');
     await sleep(4000);
@@ -47,6 +126,9 @@ async function startGame() {
     sendToLog('You suppose it is finally time to actually do something productive.');
     await sleep(4000);
     sendToLog('Magic isn\'t going to learn itself, after all.');
+    document.querySelector('.gameMainCenter').classList.add('border');
+    document.querySelector('.researchTab').classList.add('show');
     document.querySelector('.centerContent').classList.add('show');
+
 }
 startGame();
